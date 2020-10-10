e2aa7be0b938b5bb11bbdd25036b01495547e3e2
SaharatPr <saharat.darm@gmail.com>
2020-10-10 23:55:46 +0700

    new upload

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const jwt = require("jwt-simple");
 class Index{
-    
+    constructor()
+    {
+
+    }
+
+    serverStart()
+    {
+        const middleware = (req, res, next)=>{
+            if(req.headers.authorization == "saharat")
+            {
+                next();
+            }else{
+                res.json("xxxx");
+            }
+        }
+        app.get('/', middleware, (req, res) => {
+            const payload = {
+                data: "XXXXSSS",
+                iat: new Date().getTime()//มาจากคำว่า issued at time (สร้างเมื่อ)
+             };
+             const SECRET = "MY_SECRET_KEY"; //ในการใช้งานจริง คีย์นี้ให้เก็บเป็นความลับ
+             res.send(jwt.encode(payload, SECRET));
+          })
+        this.openPort(80);
+    }
+
+    openPort(port)
+    {
+        app.listen(port,()=>{`Open port ${port}`})
+    }
 }
+
+const start = new Index();
+start.serverStart();
